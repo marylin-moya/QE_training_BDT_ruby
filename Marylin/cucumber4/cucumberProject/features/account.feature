4b9f23f536fbb66d83384fe6af6028a48d522cc3
marylin <marylinmoya@gmail.com>
2017-04-17 22:30:45 +0000

    more tests

---
+++ Marylin/cucumber4/cucumberProject/features/account.feature
@@ -0,0 +1,4 @@
+Feature: Accounts
+Scenario: Accounts with debit
+Given I have $100 in my Account
+Given I have $250 in my Account
---
+++ Marylin/cucumber4/cucumberProject/features/step_definitions/account_steps.rb
@@ -0,0 +1,4 @@
+#Given(/^I have \$(\d+) in my Account$/) do |arg1|
+Given(/^I have \$(100|250) in my Account$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
