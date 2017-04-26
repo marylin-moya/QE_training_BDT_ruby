f64bb291f0c9d5626a43e50fe950834afd60d0d2
marylin <marylinmoya@gmail.com>
2017-04-26 22:43:49 +0000

    x

--- Marylin/cucumber7/Project/features/create_user.feature
+++ Marylin/cucumber7/Project/features/create_user.feature
@@ -8,11 +8,18 @@ When I send a POST request to /user.json with json
 """
     {
   "UserObject": {
-    	"Email": "use123r@email.com",
-    	"FullName": "Joe Blow",
+    	"Email": "lisa123@email.com",
+    	"FullName": "Lisa",
     	"Password": "pASswoRd"
 	  }
 	}
 """
 
 Then I expect HTTP code 200
+And I expect JSON equal to
+      """
+      {
+       "ErrorMessage": "Invalid Email Address",
+       "ErrorCode": 307
+      }
+      """
--- Marylin/cucumber7/Project/features/step_definitions/users_steps.rb
+++ Marylin/cucumber7/Project/features/step_definitions/users_steps.rb
@@ -5,6 +5,8 @@ end
 #When(/^I send a POST request to \/user\.json with json$/) do |string|
 When /^I send a (PUT|POST) request to (.*?) with json$/ do |method, end_point, json_text|
   http_request = Rest_service.get_request(method, end_point)
+  #http_request['content-type'] = 'application/json'
+  #http_request['accept'] = 'application/json'
   http_request.body = json_text
   @http_response = Rest_service.execute_request(@http_connection, http_request)
   @last_json = @http_response.body
@@ -12,4 +14,8 @@ end
 
 Then /^I expect HTTP code (\d+)$/ do |http_code|
   expect(@http_response.code).to eql(http_code)
+end
+
+Then /^I expect JSON equal to$/ do |json_text|
+  expect(@last_json).to be_json_eql json_text
 end
